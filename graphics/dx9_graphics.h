ec89060efd41c3c73f850be6b3803d67bcd72a07
Alopex6414 <1029535012@qq.com>
2019-12-07 21:44:05 +0800

    Append private variables.

--- graphics/dx9_graphics.h
+++ graphics/dx9_graphics.h
@@ -5,8 +5,16 @@
 #ifndef TWILIGHT_DX9_GRAPHICS_H
 #define TWILIGHT_DX9_GRAPHICS_H
 
-class dx9_graphics
-{
+#include <d3d9.h>
+#include "dx9_graphics.h"
+
+class dx9_graphics {
+private:
+    IDirect3D9* m_pD3D9;
+    IDirect3DDevice9* m_pD3D9Device;
+    D3DCAPS9 m_D3D9Caps;
+    D3DPRESENT_PARAMETERS m_D3D9pp;
+
 };
 
 #endif //TWILIGHT_DX9_GRAPHICS_H
